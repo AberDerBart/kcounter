92b8de23c28cee1a1ab3788ea6d7ab80122d845c
Jonas Grosse-Holz <aber@der-b.art>
2024-09-03 15:06:33 +0200

    WeightChartView: implement month view

--- src/WeightChartView.tsx
+++ src/WeightChartView.tsx
@@ -1,20 +1,52 @@
 import { FlexibleXYPlot, LineMarkSeries, XAxis, YAxis } from "react-vis";
 import { Diary } from "./types";
-import { useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import AppFrame from "./AppFrame";
 import { addDays, endOfToday, format, startOfToday } from "date-fns";
 
 import styles from "./WeightChartView.module.css";
+import Button from "./Button";
+import classNames from "classnames";
+
+type DateRange = "week" | "month";
 
 interface Props {
   diary: Diary;
 }
 
 export default function WeightChartView(props: Props) {
-  return <AppFrame nav={<div />} main={<WeightChart {...props} />} />;
+  const [range, setRange] = useState<DateRange>("week");
+
+  return (
+    <AppFrame
+      nav={
+        <div className={styles.Header}>
+          <Button
+            className={classNames(
+              styles.RangeButton,
+              range === "week" && styles.active
+            )}
+            onClick={() => setRange("week")}
+          >
+            Woche
+          </Button>
+          <Button
+            className={classNames(
+              styles.RangeButton,
+              range === "month" && styles.active
+            )}
+            onClick={() => setRange("month")}
+          >
+            Monat
+          </Button>
+        </div>
+      }
+      main={<WeightChart {...props} range={range} />}
+    />
+  );
 }
 
-export function WeightChart({ diary }: Props) {
+export function WeightChart({ diary, range }: Props & { range: DateRange }) {
   const data = useMemo(() => {
     return Object.entries(diary)
       .flatMap(([date, v]) =>
@@ -23,16 +55,14 @@ export function WeightChart({ diary }: Props) {
       .sort((l, r) => (l.x < r.x ? -1 : 1));
   }, [diary]);
 
-  const [range, setRange] = useState<"week" | "month">("week");
-
   const tickValues = useMemo(() => {
     if (range === "week") {
       return Array.from({ length: 7 }).map((_, i) =>
         addDays(startOfToday(), i - 6).getTime()
       );
     }
-    return Array.from({ length: 11 }).map((_, i) =>
-      addDays(startOfToday(), i * 3 - 30).getTime()
+    return Array.from({ length: 5 }).map((_, i) =>
+      addDays(startOfToday(), i * 7 - 30).getTime()
     );
   }, [range]);
 
@@ -47,13 +77,21 @@ export function WeightChart({ diary }: Props) {
     );
   }, [data, range]);
 
+  const xTickFormat = useCallback(
+    (tick: number) => {
+      if (range === "week") {
+        return format(tick, "E");
+      }
+
+      return format(tick, "d.M.");
+    },
+    [range]
+  );
+
   return (
     <div className={styles.Wrapper}>
       <FlexibleXYPlot>
-        <XAxis
-          tickValues={tickValues}
-          tickFormat={(tick) => format(tick, "E")}
-        />
+        <XAxis tickValues={tickValues} tickFormat={xTickFormat} />
         <YAxis title="kg" />
         <LineMarkSeries data={filteredData} lineStyle={{ fill: "none" }} />
       </FlexibleXYPlot>
